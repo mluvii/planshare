d7f2de844cc5573365d0415a94c8c6cc5bb84ae3
Ondřej Vondra <ondrej.vondra@live.com>
2020-12-02 13:47:55 +0100

    save to and load from redis

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/fsnotify/fsnotify v1.4.9
+	github.com/go-redis/redis/v8 v8.4.0 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 )
--- backend/plansharesvc.go
+++ backend/plansharesvc.go
@@ -1,18 +1,80 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
-
-	"github.com/gorilla/mux"
 )
+
+var topCtx = context.Background()
+
+var rdb = func() *redis.Client {
+	addr, configured := os.LookupEnv("REDIS_ADDR")
+	if !configured {
+		addr = "localhost:6379"
+	}
+
+	db, dbEnvErr := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if dbEnvErr != nil {
+		db = 0
+	}
+
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       db,
+	})
+}()
 
 func handleSave(res http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
 		res.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var js json.RawMessage
+	err = json.Unmarshal(body, &js)
+	if err != nil {
+		log.Println("Cannot unmarshal json", err)
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	ctx := context.WithValue(topCtx, 0, req.Header.Get("X-Request-ID"))
+
+	id := strings.ToLower(strings.Replace(uuid.New().String(), "-", "", -1))
+
+	err = rdb.HSet(ctx, "planshare", id, body).Err()
+	if err != nil {
+		log.Println("Failed to save", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	responseBytes, _ := json.Marshal(&struct {
+		Id string `json:"id"`
+	}{
+		Id: id,
+	})
+
+	res.WriteHeader(http.StatusCreated)
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(responseBytes)
 }
 
 func handleLoad(res http.ResponseWriter, req *http.Request) {
@@ -20,23 +82,55 @@ func handleLoad(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+
+	vars := mux.Vars(req)
+	id, found := vars["id"]
+	if !found {
+		http.NotFound(res, req)
+		return
+	}
+
+	ctx := context.WithValue(topCtx, 0, req.Header.Get("X-Request-ID"))
+
+	val, err := rdb.HGet(ctx, "planshare", id).Result()
+	if err != nil {
+		log.Println("Failed to load", err)
+		http.NotFound(res, req)
+		return
+	}
+
+	var js json.RawMessage
+	err = json.Unmarshal([]byte(val), &js)
+	if err != nil {
+		log.Println("Cannot unmarshal json", err)
+		http.NotFound(res, req)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write([]byte(val))
 }
 
 func listenProxy() {
 	r := mux.NewRouter()
-	r.HandleFunc("/save", handleSave)
-	r.HandleFunc("/{:id}", handleLoad)
+	r.HandleFunc("/{id}", handleLoad).Methods("GET")
+	r.HandleFunc("/", handleSave).Methods("POST")
+
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	server := &http.Server{
-		Addr:         "127.0.0.1:8080",
+		Addr:         "127.0.0.1:" + port,
 		Handler:      r,
 		ReadTimeout:  time.Duration(60) * time.Second,
 		WriteTimeout: time.Duration(60) * time.Second,
 	}
 
-	error := server.ListenAndServe()
-	if error != nil {
-		log.Fatalf("Failed to listen and serve: %s\n", error.Error())
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Failed to listen and serve: %s\n", err.Error())
 	}
 }
 
